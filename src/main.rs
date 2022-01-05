1c3661b54339b358f42d9d58f95d67797eb0bb0c
Jonny Wildey <jonnywildey@gmail.com>
2022-01-05 03:13:37 +0000

    something like delay

--- src/main.rs
+++ src/main.rs
@@ -48,3 +48,25 @@ fn rev<T: std::marker::Copy>(rev: &Vec<T>) -> Vec<T> {
     }
     new_rev.to_vec()
 }
+
+fn delay<T: std::marker::Copy + std::ops::AddAssign>(
+    rev: &Vec<T>,
+    delay_length: usize,
+    repeats: u32,
+) -> Vec<T> {
+    let mut new_rev = rev.clone();
+
+    let length = rev.len();
+
+    for i in 1..length {
+        let delay_i = i - delay_length;
+        if i >= delay_length {
+            new_rev[i] += rev[delay_i]
+        }
+    }
+
+    if (repeats > 0) {
+        new_rev = delay(&new_rev, delay_length, repeats - 1);
+    }
+    new_rev.to_vec()
+}
