d1ec2bbd6eda72d71cf0d8dffd10addf30d1c0a5
Jonny Wildey <jonnywildey@gmail.com>
2022-01-12 01:13:50 +0000

    wip

--- permute-ui/src/lib.rs
+++ permute-ui/src/lib.rs
@@ -2,10 +2,11 @@ use neon::prelude::*;
 use permute::permute_files::*;
 use permute::process::*;
 
+// See https://github.com/neon-bindings/examples/blob/b603cd9bc5bda13c9392e1c8ed79d386f5063066/examples/async-sqlite/src/lib.rs
+
 #[derive(Debug)]
 struct ApplicationState {
     pub permute_params: PermuteFilesParams,
-    pub progress: i32,
 }
 
 impl Finalize for ApplicationState {}
@@ -50,10 +51,7 @@ impl ApplicationState {
 
         let progress = 0;
 
-        Self {
-            permute_params,
-            progress,
-        }
+        Self { permute_params }
     }
 }
 
@@ -83,6 +81,35 @@ impl ApplicationState {
         permute_files(state.permute_params.clone());
         Ok(cx.undefined())
     }
+
+    fn set_update_handler_js(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+        let state = cx.argument::<JsBox<ApplicationState>>(0)?;
+        let state = state.downcast_or_throw::<JsBox<ApplicationState>, _>(&mut cx)?;
+
+        let update_handler = cx.argument::<JsFunction>(1)?.root(&mut cx);
+
+        let update_permute_node_progress =
+            move |permutation: Permutation, name: PermuteNodeName, event: PermuteNodeEvent| {
+                let update_handler = update_handler.into_inner(&mut cx);
+                let progress = match event {
+                    PermuteNodeEvent::NodeProcessStarted => String::default(),
+                    PermuteNodeEvent::NodeProcessComplete => {
+                        let percentage_progress: f64 = ((permutation.node_index as f64 + 1.0)
+                            / permutation.processors.len() as f64)
+                            * 100.0;
+                        format!("{}%", percentage_progress.round())
+                    }
+                };
+                let progress = cx.string(progress.to_owned());
+                let this = cx.undefined();
+                let args: Vec<Handle<JsString>> = vec![progress];
+                update_handler.call(&mut cx, this, args).unwrap();
+            };
+
+        state.permute_params.update_permute_node_progress = update_permute_node_progress;
+
+        Ok(cx.undefined())
+    }
 }
 
 #[neon::main]
@@ -90,6 +117,7 @@ fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("init", ApplicationState::init_js)?;
     cx.export_function("getState", ApplicationState::get_state_js)?;
     cx.export_function("run", ApplicationState::run_js)?;
+    cx.export_function("setUpdateHandler", ApplicationState::set_update_handler_js)?;
 
     Ok(())
 }
