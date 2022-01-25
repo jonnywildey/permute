b17a2617cd725746b04d20fbf52e8d289232f873
Jonny Wildey <jonnywildey@gmail.com>
2022-01-25 19:39:08 +0000

    add back to cmd line

--- permute-core/src/main.rs
+++ permute-core/src/main.rs
@@ -49,14 +49,14 @@ fn main() {
     );
 
     let processor_pool: Vec<PermuteNodeName> = vec![
-        // PermuteNodeName::Reverse,
-        // PermuteNodeName::MetallicDelay,
-        // PermuteNodeName::RhythmicDelay,
-        // PermuteNodeName::HalfSpeed,
-        // PermuteNodeName::DoubleSpeed,
-        // PermuteNodeName::Wow,
-        // PermuteNodeName::Flutter,
-        // PermuteNodeName::Chorus,
+        PermuteNodeName::Reverse,
+        PermuteNodeName::MetallicDelay,
+        PermuteNodeName::RhythmicDelay,
+        PermuteNodeName::HalfSpeed,
+        PermuteNodeName::DoubleSpeed,
+        PermuteNodeName::Wow,
+        PermuteNodeName::Flutter,
+        PermuteNodeName::Chorus,
         PermuteNodeName::Phaser,
     ];
 
