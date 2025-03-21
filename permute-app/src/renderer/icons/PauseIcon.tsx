b9f1373b3b126956c10d9ccbebc93c62337c831a
Jonny Wildey <jonnywildey@gmail.com>
2025-03-21 01:38:43 +0000

    feat: icons in component

--- permute-app/src/renderer/icons/PauseIcon.tsx
+++ permute-app/src/renderer/icons/PauseIcon.tsx
@@ -1,4 +1,20 @@
-import pause from '../../img/icons/pause-button.svg';
+const pauseContent = <g fill="currentColor">
+  <g>
+    <path d="M150.001,0c-82.838,0-150,67.159-150,150c0,82.838,67.162,150.003,150,150.003c82.843,0,150-67.165,150-150.003
+      C300.001,67.159,232.846,0,150.001,0z M134.41,194.538c0,9.498-7.7,17.198-17.198,17.198s-17.198-7.7-17.198-17.198V105.46
+      c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z M198.955,194.538c0,9.498-7.701,17.198-17.198,17.198
+      c-9.498,0-17.198-7.7-17.198-17.198V105.46c0-9.498,7.7-17.198,17.198-17.198s17.198,7.7,17.198,17.198V194.538z"/>
+  </g>
+</g>;
 
-export const PauseIcon = () => <img height={10.5} width={10.5} src={pause} />;
-export const LargePauseIcon = () => <img height={16} width={16} src={pause} />;
+export const PauseIcon = () => <svg version="1.1" className="chakra-icon" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  width="9.5px" height="9.5px" viewBox="0 0 300.003 300.003"
+>
+  {pauseContent}
+</svg>;
+
+export const LargePauseIcon = () => <svg version="1.1" className="chakra-icon" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  width="16px" height="16px" viewBox="0 0 300.003 300.003"
+>
+  {pauseContent}
+</svg>;
--- permute-app/src/renderer/icons/PlayIcon.tsx
+++ permute-app/src/renderer/icons/PlayIcon.tsx
@@ -1,4 +1,22 @@
 import play from '../../img/icons/play-button.svg';
 
-export const PlayIcon = () => <img height={9.5} width={9.5} src={play} />;
-export const LargePlayIcon = () => <img height={16} width={16} src={play} />;
+const playContent = <g fill="currentColor">
+  <g>
+    <path d="M204.11,0C91.388,0,0,91.388,0,204.111c0,112.725,91.388,204.11,204.11,204.11c112.729,0,204.11-91.385,204.11-204.11
+			C408.221,91.388,316.839,0,204.11,0z M286.547,229.971l-126.368,72.471c-17.003,9.75-30.781,1.763-30.781-17.834V140.012
+			c0-19.602,13.777-27.575,30.781-17.827l126.368,72.466C303.551,204.403,303.551,220.217,286.547,229.971z"/>
+  </g>
+</g>
+
+export const PlayIcon = () => <svg version="1.1" className="chakra-icon" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  width="9.5px" height="9.5px" viewBox="0 0 408.221 408.221"
+>
+  {playContent}
+</svg>
+  ;
+export const LargePlayIcon = () => <svg version="1.1" className="chakra-icon" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  width="16px" height="16px" viewBox="0 0 408.221 408.221"
+>
+  {playContent}
+</svg>
+  ;
